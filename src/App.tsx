9ac182dc72b2fe9571a17d605fa98016fe4b2010
MrLevi1112 <ido1112levi@gmail.com>
2025-12-15 16:41:48 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Crash2Cost</h1>
+      <p>Frontend is running!</p>
+    </div>
+  );
+}
+
+export default App;
